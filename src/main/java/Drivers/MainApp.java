b8c57e1cfb67005baa613b95b015894e8c292f44
Mishuk <dd31137@uga.edu>
2019-01-10 17:47:03 -0500

    Added UI-Support

--- src/main/java/Drivers/MainApp.java
+++ src/main/java/Drivers/MainApp.java
@@ -1,40 +1,8 @@
 package Drivers;
 
-import Controllers.Finder;
-import Entities.Word;
-
-import java.net.ProtocolException;
-import java.util.Scanner;
-
 public class MainApp {
-
-    Word answer =null;
-    Finder finder = new Finder();
-    public boolean queryCheck(String s) {
-        if (s.length()>0)
-            return true;
-        System.out.println("Bad Query.Recheck Again [Query]: " +s);
-        return false;
-    }
-
-    public void sendQuery(String query) throws ProtocolException {
-        if (queryCheck(query))
-        finder.presenter(query);
-
-    }
-    public static void main (String [] args) throws ProtocolException {
-        MainApp m = new MainApp();
-        while (true) {
-            System.out.println("Insert your Query Word below:");
-            Scanner scanner = new Scanner(System.in);
-            String query = scanner.nextLine();
-            if (query.equals("-1"))
-                break;
-            if (m.queryCheck(query)) {
-                m.sendQuery(query);
-            }
-        }
-        System.out.println("End of Program\n");
+     static UI ui = new UI();
+    public static void main(String[] args) {
+        ui.start();
     }
 }
-
--- src/main/java/Drivers/UI.java
+++ src/main/java/Drivers/UI.java
@@ -20,99 +20,87 @@ import javafx.stage.WindowEvent;
 
 import java.net.ProtocolException;
 
-public class UI extends Application{
+public class UI extends Application {
 
-        Finder finder = new Finder();
+    Finder finder = new Finder();
 
     public boolean queryCheck(String s) {
-        if (s.length()>0)
+        if (s.length() > 0)
             return true;
-        System.out.println("Bad Query.Recheck Again [Query]: " +s);
+        System.out.println("Bad Query.Recheck Again [Query]: " + s);
         return false;
     }
 
-    public void sendQuery(String query) throws ProtocolException {
-        if (queryCheck(query))
-            finder.presenter(query);
 
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Robo Dictionary");
+
+        GridPane grid = new GridPane();
+        grid.setVgap(6);
+        grid.setHgap(10);
+
+        Scene scene = new Scene(new Group(), 650, 400);
+        final TextField queryBox = new TextField();
+        queryBox.setText("Query");
+        queryBox.clear();
+        final TextArea definition = new TextArea();
+        definition.setMinWidth(50);
+        definition.setPrefWidth(400);
+        definition.setMaxWidth(400);
+        definition.setWrapText(true);
+        definition.setText("Answer Will be Displayed Here");
+        definition.prefColumnCountProperty().bind(definition.textProperty().length());
+        definition.setWrapText(true);
+        definition.setEditable(false);
+        definition.clear();
+
+
+        grid.setPadding(new Insets(5, 5, 5, 5));
+        grid.add(new Label("Insert Query Word Here"), 0, 0);
+        grid.add(queryBox, 1, 0);
+        grid.add(new Label("Details:"), 1, 2);
+        grid.add(definition, 1, 3);
+
+
+        Group root = (Group) scene.getRoot();
+        root.getChildren().add(grid);
+        root.prefWidth(500);
+        root.prefHeight(500);
+        /**
+         * Button Search
+         */
+        final Button search = new Button("Search");
+        grid.add(search, 1, 2);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent event) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+
+        search.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                definition.setText("Searching.....");
+                String query = queryBox.getText();
+                if (!queryCheck(query))
+                    definition.setText("Bad Query. Please DoubleCheck.\nError Causing Query: " + query);
+                else
+                    try {
+                        definition.setText(finder.uiPresenter(query));
+                    } catch (ProtocolException e1) {
+                        e1.printStackTrace();
+                    }
+            }
+        });
     }
 
-        @Override
-        public void start(Stage primaryStage)  {
-            primaryStage.setTitle("Robo Dictionary");
-
-            GridPane grid = new GridPane();
-            grid.setVgap(6);
-            grid.setHgap(10);
-
-            Scene scene = new Scene(new Group(), 650, 400);
-            final TextField queryBox = new TextField ();
-            queryBox.setText("Query");
-            queryBox.clear();
-            final TextArea definition = new TextArea();
-            definition.setMinWidth(50);
-            definition.setPrefWidth(400);
-            definition.setMaxWidth(400);
-            definition.setWrapText(true);
-            definition.setText("Answer Will be Displayed Here");
-            definition.prefColumnCountProperty().bind(definition.textProperty().length());
-            definition.setWrapText(true);
-            definition.setEditable(false);
-            definition.clear();
-
-
-
-            grid.setPadding(new Insets(5, 5, 5, 5));
-            grid.add(new Label("Insert Query Word Here"), 0, 0);
-            grid.add(queryBox, 1, 0);
-            grid.add(new Label("Details:"), 1, 2);
-            grid.add(definition, 1, 3);
-
-
-            Group root = (Group) scene.getRoot();
-            root.getChildren().add(grid);
-            root.prefWidth(500); root.prefHeight(500);
-            /**
-             * Button Search
-             */
-            final Button search = new Button("Search");
-            grid.add (search, 1, 2 );
-            primaryStage.setScene(scene);
-            primaryStage.show();
-
-            primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-                                               public void handle(WindowEvent event) {
-                                                   Platform.exit();
-                                                   System.exit(0);
-                                               }
-                                               });
-
-            search.setOnAction(new EventHandler<ActionEvent>() {
-                 public void handle(ActionEvent e) {
-                     definition.setText("Searching.....");
-                     String query = queryBox.getText();
-                     if (!queryCheck(query))
-                         definition.setText("Bad Query. Please DoubleCheck.\nError Causing Query: "+query);
-                     else
-                     try {
-                         definition.setText(finder.uiPresenter(query));
-                     }
-                     catch (ProtocolException e1) { e1.printStackTrace(); }
-                }
-            });
-        }
-
-
-    /**
-     * Button Even Handler: search
-     * @param args
-     */
-
-
-
-    public static void main(String[] args) {
-            Application.launch(args);
-        }
+    public void start() {
+        Application.launch();
     }
+}
 
 
